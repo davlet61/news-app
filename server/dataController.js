2a9e6064d48e2b59119eb94df9c789e4f8cce849
davlet61 <30911739+davlet61@users.noreply.github.com>
2022-03-18 12:29:03 +0100

    fix .env

--- server/dataController.js
+++ server/dataController.js
@@ -3,17 +3,18 @@ const fs = require('fs');
 const axios = require('axios');
 require('dotenv').config();
 
+const date = new Date();
+const today = date.toISOString().substring(0, 10);
+
 const { API_KEY } = process.env;
+const { TEXT_TO_SPEACH } = process.env;
 const basePathToData = path.join(__dirname, '../server/mock/news.json');
 
 const getJsonData = filePath => JSON.parse(fs.readFileSync(filePath, 'utf-8'));
 
 const getMockData = (_req, res) => {
   const data = getJsonData(basePathToData);
-  setTimeout(
-    () => res.set({ 'Access-Control-Allow-Origin': '*' }).json(data),
-    100,
-  );
+  setTimeout(() => res.json(data), 100);
 };
 
 const getData = async (req, res) => {
@@ -21,9 +22,9 @@ const getData = async (req, res) => {
   await process.nextTick(() => {});
   try {
     const response = await axios.get(
-      `https://newsapi.org/v2/everything?q=${q}&from=2022-02-16&sortBy=publishedAt&language=en`,
+      `https://newsapi.org/v2/everything?q=${q}&from=${today}&sortBy=publishedAt&language=en`,
       {
-        headers: { 'X-Api-Key': API_KEY, 'Access-Control-Allow-Origin': '*' },
+        headers: { 'X-Api-Key': API_KEY },
       },
     );
     res.json(response.data);
@@ -32,4 +33,53 @@ const getData = async (req, res) => {
   }
 };
 
-module.exports = { getMockData, getData };
+const getLatest = async (req, res) => {
+  await process.nextTick(() => {});
+  try {
+    const response = await axios.get(
+      'https://newsapi.org/v2/top-headlines?country=us&sortBy=publishedAt',
+      {
+        headers: { 'X-Api-Key': API_KEY },
+      },
+    );
+    res.json(response.data);
+  } catch (err) {
+    res.status(500).json({ message: 'Ops! Something went wrong' });
+  }
+};
+
+const textToSpeach = async (req, res) => {
+  const q = req.query.search;
+  const options = {
+    method: 'GET',
+    url: 'https://voicerss-text-to-speech.p.rapidapi.com/',
+    params: {
+      hl: 'en-us',
+      src: `${q}`,
+      key: TEXT_TO_SPEACH,
+      f: '16khz_16bit_stereo',
+      c: 'mp3',
+      r: '0',
+      b64: true,
+    },
+    headers: {
+      'x-rapidapi-host': 'voicerss-text-to-speech.p.rapidapi.com',
+      'x-rapidapi-key': 'e2cab1cc1emsh18c45ea14186247p1663c9jsn5ead56b07558',
+    },
+  };
+
+  await process.nextTick(() => {});
+  try {
+    const response = await axios.request(options);
+    res.set({ 'content-type': 'audio/mpeg' }).send(response.data);
+  } catch (err) {
+    res.status(500).json({ message: 'Ops! Something went wrong' });
+  }
+};
+
+module.exports = {
+  getMockData,
+  getData,
+  textToSpeach,
+  getLatest,
+};
--- server/router.js
+++ server/router.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const { getData, getMockData } = require('./dataController');
+const {
+  getData,
+  getMockData,
+  textToSpeach,
+  getLatest,
+} = require('./dataController');
 
-const controller = process.env.NODE_ENV === 'development'
-  ? getMockData
-  : getData;
+const controller = process.env.NODE_ENV === 'development' ? getMockData : getData;
 
 const router = express.Router();
 
 router.get('/news', controller);
+router.get('/news/latest', getLatest);
+router.get('/voice', textToSpeach);
 
 module.exports = router;
